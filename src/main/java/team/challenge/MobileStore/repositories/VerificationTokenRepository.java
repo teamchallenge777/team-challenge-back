53bac21efdd3f9174638fec44cbde2832fb93b98
Romych <lutsiv55@gmail.com>
2024-01-18 16:02:58 +0200

    Create scheduler for deleting not active tokens.

--- src/main/java/team/challenge/MobileStore/repositories/VerificationTokenRepository.java
+++ src/main/java/team/challenge/MobileStore/repositories/VerificationTokenRepository.java
@@ -4,10 +4,13 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import team.challenge.MobileStore.model.UserModel;
 import team.challenge.MobileStore.model.VerificationToken;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 public interface VerificationTokenRepository extends MongoRepository<VerificationToken, String> {
     Optional<VerificationToken> findByToken(String token);
     Optional<VerificationToken> findByUser(UserModel user);
+    List<VerificationToken> findAllByExpiryDateBefore(Date date);
 
 }
