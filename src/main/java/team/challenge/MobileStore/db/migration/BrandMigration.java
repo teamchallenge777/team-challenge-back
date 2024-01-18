64743876e620a2c689284925b9f8fb4d204c33b0
timcol1 <avlyakulovtimur@gmail.com>
2024-01-18 19:12:34 +0200

    finish first part of catalogue

--- src/main/java/team/challenge/MobileStore/db/migration/BrandMigration.java
+++ src/main/java/team/challenge/MobileStore/db/migration/BrandMigration.java
@@ -1,4 +1,4 @@
-package team.challenge.MobileStore.dbmigration;
+package team.challenge.MobileStore.db.migration;
 
 import com.github.cloudyrock.mongock.ChangeLog;
 import com.github.cloudyrock.mongock.ChangeSet;
@@ -13,16 +13,18 @@ public class BrandMigration {
     @ChangeSet(order = "001", id = "init_first_brands", author = "Romych")
     public void initBrands(BrandRepository brandRepository){
         List<Brand> brands = List.of(
-                new Brand("Apple"),
-                new Brand("Samsung"),
-                new Brand("Xiaomi"),
-                new Brand("Nokia"),
                 new Brand("OnePlus"),
-                new Brand("ZTE"),
                 new Brand("OPPO"),
-                new Brand("Google Pixel"),
+                new Brand("Motorola"),
+                new Brand("Nokia"),
+                new Brand("Apple"),
+                new Brand("Xiaomi"),
+                new Brand("Samsung"),
+                new Brand("Nothing"),
                 new Brand("Realme"),
-                new Brand("Motorola")
+                new Brand("Google Pixel"),
+                new Brand("Redmi"),
+                new Brand("ZTE")
         );
         brandRepository.saveAll(brands);
     }
